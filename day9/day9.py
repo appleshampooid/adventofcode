212db11a03ad8dea6e5cf425440950880427ce57
Fullname McFullerson <appleshampooid@gmail.com>
2020-12-09 08:58:33 -0500

    day9 !!

---
+++ day9/day9.py
@@ -0,0 +1,37 @@
+import sys
+from itertools import combinations
+preamble_size = int(sys.argv[2])
+cipher = []
+
+def valid(x, addends):
+    # print(f'x is {x}, addends are {addends}')
+    for (y, z) in combinations(addends, 2):
+        if y + z == x:
+            return True
+    return False
+
+invalid = None
+with open(sys.argv[1]) as xmas_input:
+    for i, line in enumerate(xmas_input):
+        x = int(line)
+        cipher.append(x)
+        if i >= preamble_size and not invalid:
+            if not valid(x, cipher[i - preamble_size:i]):
+                invalid = x
+                print(f'Part 1: the invalid number is {invalid}')
+
+for i, x in enumerate(cipher):
+    sum = x
+    smallest = x
+    largest = x
+    for y in cipher[1+i:]:
+        if y > largest:
+            largest = y
+        if y < smallest:
+            smallest = y
+        sum += y
+        if sum == invalid:
+            print(f'Part 2: sum is {smallest + largest} from {smallest} and {largest}; range is from {x} to {y}')
+            sys.exit(0)
+        elif sum > invalid:
+            break
