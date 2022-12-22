3336fce43788365eebcb7d6db334882c683541b6
Fullname McFullerson <appleshampooid@gmail.com>
2022-12-22 08:34:28 -0500

    2022 day 2

---
+++ 2022/day02/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	//"regexp"
+)
+
+const (
+	rock     int = 1
+	paper        = 2
+	scissors     = 3
+)
+
+var mapper map[string]int
+
+func main() {
+	flag.Parse()
+	if flag.NArg() != 1 {
+		fmt.Println("Need an input file")
+		os.Exit(1)
+	}
+	fileName := flag.Args()[0]
+	inputFile, err := os.Open(fileName)
+	if err != nil {
+		fmt.Printf("Error opening file %s\n", fileName)
+		os.Exit(1)
+	}
+	defer inputFile.Close()
+
+	mapper = map[string]int{"A": rock,
+		"X": rock,
+		"B": paper,
+		"Y": paper,
+		"C": scissors,
+		"Z": scissors}
+	scanner := bufio.NewScanner(inputFile)
+	totalScoreDay1 := 0
+	totalScoreDay2 := 0
+	for scanner.Scan() {
+		plays := strings.Split(scanner.Text(), " ")
+		totalScoreDay1 += scoreRound(mapper[plays[0]], mapper[plays[1]])
+		totalScoreDay2 += scoreRound(mapper[plays[0]], determineMyPlay(mapper[plays[0]], plays[1]))
+	}
+	fmt.Printf("Total score for day 1 is %d\n", totalScoreDay1)
+	fmt.Printf("Total score for day 2 is %d\n", totalScoreDay2)
+}
+
+func scoreRound(theirs, mine int) int {
+	score := 0
+	if (mine == rock && theirs == scissors) || (mine == paper && theirs == rock) || (mine == scissors && theirs == paper) {
+		score += 6
+	} else if mine == theirs {
+		score += 3
+	}
+	score += mine
+	return score
+}
+
+func determineMyPlay(theirs int, result string) int {
+	switch result {
+	case "X":
+		switch theirs {
+		case rock:
+			return scissors
+		case paper:
+			return rock
+		case scissors:
+			return paper
+		}
+	case "Y":
+		return theirs
+	case "Z":
+		switch theirs {
+		case rock:
+			return paper
+		case paper:
+			return scissors
+		case scissors:
+			return rock
+		}
+	}
+	panic("whoops")
+}
